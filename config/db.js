99393db9238bda2e56ef2316717b9c4821a60c03
Arham Sameel <arhamsameel3@gmail.com>
2025-02-20 10:49:01 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,30 @@
-const mongoose = require('mongoose');
 
-function connecttoDB() {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to DB');
-    })
-    .catch(err => {
-        console.error('Database connection error:', err);
-    });
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.qp0fx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+function connectToDB(){
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
+}
+//run().catch(console.dir);
 
-module.exports = connecttoDB;
+module.exports = connectToDB;
